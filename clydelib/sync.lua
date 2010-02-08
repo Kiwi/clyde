23f9492639c462716897227e8f480053338a1a84
Kiwi <envy1988@gmail.com>
2010-02-07 19:31:30 -0600

    [sync.lua] -Ss colors

--- clydelib/sync.lua
+++ clydelib/sync.lua
@@ -193,9 +193,12 @@ local function sync_search(syncs, targets)
                 if (not config.quiet) then
                     local dbcolor = dbcolors[db:db_get_name()] or C.magb
                     if (localdb:db_get_pkg(pkg:pkg_get_name())) then
+<<<<<<< HEAD
                         printf("%s%s %s %s", dbcolor(db:db_get_name().."/"), C.bright(pkg:pkg_get_name()), C.greb(pkg:pkg_get_version()), yelbold.."[installed]"..C.reset)
                     else
                         printf("%s%s %s", dbcolor(db:db_get_name().."/"), C.bright(pkg:pkg_get_name()), C.greb(pkg:pkg_get_version()))
+=======
+>>>>>>> [sync.lua] -Ss colors
                     end
                 else
                     printf("%s", C.bright(pkg:pkg_get_name()))
