88ede99aa85e75b6517a7dad07dbb1b5348f9469
Kiwi <envy1988@gmail.com>
2010-03-31 22:45:57 -0500

    [transinit.lua] Update to use pm_errno() and eprintf instead of the old hack and just printf

--- clydelib/transinit.lua
+++ clydelib/transinit.lua
@@ -2,15 +2,18 @@ module(..., package.seeall)
 local alpm = require "lualpm"
 local util = require "clydelib.util"
 local callback = require "clydelib.callback"
+local utilcore = require "clydelib.utilcore"
 local printf = util.printf
+local eprintf = util.eprintf
+local g = utilcore.gettext
 
 function trans_init(ttype, flags)
     local ret  = alpm.trans_init(ttype, flags, callback.cb_trans_evt, callback.cb_trans_conv, callback.cb_trans_progress)
     if (ret == -1) then
-        printf("error: failed to init transaction(%s)\n", alpm.strerrorlast())
-        if (alpm.strerrorlast() == "unable to lock database") then
-            printf("  if you're sure a package manager is not already\n"..
-                  "  running, you can remove %s\n", alpm.option_get_lockfile())
+        eprintf("LOG_ERROR", g("failed to init transaction (%s)\n"), alpm.strerrorlast())
+        if (alpm.pm_errno() == "P_E_HANDLE_LOCK") then
+            io.stderr:write(string.format(g("  if you're sure a package manager is not already\n"..
+                  "  running, you can remove %s\n"), alpm.option_get_lockfile()))
         end
         return -1
     end
