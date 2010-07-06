c813c2f17be464ca54ae99b88eafde3c85f258a4
Justin Davis <jrcd83@gmail.com>
2010-07-06 11:43:30 -0700

    Remove leftover unneeded function.

--- clydelib/util.lua
+++ clydelib/util.lua
@@ -65,15 +65,6 @@ function strtrim(str)
     return str
 end
 
-function booltostring(bool)
-    -- this is no joke
-    if bool then
-        return "true"
-    else
-        return "false"
-    end
-end
-
 function basename(name)
     return name:match("/(%w+)$")
 end
