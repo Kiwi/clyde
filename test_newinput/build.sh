070fe69de57dc4e965b62744591d394ac82c276f
Justin Davis <jrcd83@gmail.com>
2010-10-24 10:27:32 -0700

    Remove Otto's tests for SIGINT fix.

--- test_newinput/build.sh
+++
@@ -1,5 +0,0 @@
-LUACFLAGS="`pkg-config --cflags lua` -shared -pedantic -std=c99 -D_FILE_OFFSET_BITS=64 -D_GNU_SOURCE"
-
-#gcc ${LUACFLAGS} -Wall lsignal.c -Wl,-soname,signal.so -o signal.so || exit 1 
-gcc ${LUACFLAGS} -Wall lsignal.c -o signal.so || exit 1 
-gcc ${LUACFLAGS} -Wall sigtest.c -o sigtestlib.so || exit 1
