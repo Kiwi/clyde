6f7fac7d029f7f5eb2b1a28c88d28ae3131ae63f
Justin Davis <jrcd83@gmail.com>
2010-11-08 00:11:09 -0800

    Move trans callbacks to callback.c. Change trans_init args to table.

--- lualpm/callback.c
+++ lualpm/callback.c
@@ -35,7 +35,7 @@ cb_lookup ( callback_key_t *key )
     lua_gettable( GlobalState, LUA_REGISTRYINDEX );
 
     if ( lua_isnil( GlobalState, -1 )) {
-        cb_log_error( key->name, "Value for callback is nil!" );
+        /* cb_log_error( key->name, "Value for callback is nil!" ); */
         return 0;
     }
     else if ( lua_type( GlobalState, -1 ) != LUA_TFUNCTION ) {
@@ -121,6 +121,9 @@ BEGIN_CALLBACK( totaldl, off_t total )
 }
 END_CALLBACK( totaldl, 1 )
 
+#undef BEGIN_CALLBACK
+#undef END_CALLBACK
+
 /* The fetch callback is tricker because it returns -1 if an error
    occurs. */
 callback_key_t cb_key_fetch = { "fetch callback" };
@@ -128,7 +131,6 @@ callback_key_t cb_key_fetch = { "fetch callback" };
 int cb_cfunc_fetch ( const char *url, const char *localpath, int force )
 {
     lua_State *L = GlobalState;
-    int count;
 
     lua_pushstring( L, url );
     lua_pushstring( L, localpath );
@@ -142,3 +144,247 @@ int cb_cfunc_fetch ( const char *url, const char *localpath, int force )
     if ( lua_isnil( L, 1 ) || !lua_isnumber( L, 1 )) { return -1; }
     return lua_tointeger( L, 1 );
 }
+
+/****************************************************************************/
+/* TRANSACTION CALLBACKS                                                    */
+
+#define BEGIN_TRANS_CALLBACK( NAME, ... ) \
+    callback_key_t transcb_key_ ## NAME = { #NAME " callback" }; \
+                                                                 \
+    void transcb_cfunc_ ## NAME ( __VA_ARGS__ )                  \
+    {                                                            \
+    lua_State *L = GlobalState;                                  \
+    lua_newtable( L );                                           \
+                                                                 \
+    if ( ! cb_lookup( &transcb_key_ ## NAME )) { return; }
+
+#define END_TRANS_CALLBACK( NAME )                  \
+    int lua_err = lua_pcall( L, 1, 0, 0 );   \
+    if ( lua_err != 0 ) {                           \
+        cb_error_handler( #NAME, lua_err );         \
+    }                                               \
+    return;                                         \
+    } /* end of transcb_cfunc */
+
+/* TRANSACTION EVENT CALLBACK ***********************************************/
+
+#define EVT_TEXT( KEY, STR )                    \
+    lua_pushstring( L, STR );                   \
+    lua_setfield( L, -2, KEY );
+
+#define EVT_NAME( NAME )     EVT_TEXT( "name", NAME )
+#define EVT_STATUS( STATUS ) EVT_TEXT( "status", STATUS )
+
+#define EVT_PKG( KEY, PKG )                     \
+    *( push_pmpkg_box( L ) ) = (pmpkg_t *)PKG;  \
+    lua_setfield( L, -2, KEY );
+
+BEGIN_TRANS_CALLBACK( event, pmtransevt_t event, void *arg_one, void *arg_two )
+{
+    switch ( event ) {
+    case PM_TRANS_EVT_CHECKDEPS_START:
+        EVT_NAME("checkdeps")
+        EVT_STATUS("start")
+        break;
+    case PM_TRANS_EVT_CHECKDEPS_DONE:
+        EVT_NAME("checkdeps")
+        EVT_STATUS("done")
+        break;
+    case PM_TRANS_EVT_FILECONFLICTS_START:
+        EVT_NAME("fileconflicts")
+        EVT_STATUS("start")
+        break;
+	case PM_TRANS_EVT_FILECONFLICTS_DONE:
+        EVT_NAME("fileconflicts")
+        EVT_STATUS("done")
+        break;
+	case PM_TRANS_EVT_RESOLVEDEPS_START:
+        EVT_NAME("resolvedeps")
+        EVT_STATUS("start")
+        break;
+	case PM_TRANS_EVT_RESOLVEDEPS_DONE:
+        EVT_NAME("resolvedeps")
+        EVT_STATUS("done")
+        break;
+	case PM_TRANS_EVT_INTERCONFLICTS_START:
+        EVT_NAME("interconflicts")
+        EVT_STATUS("start")
+        break;
+	case PM_TRANS_EVT_INTERCONFLICTS_DONE:
+        EVT_NAME("interconflicts")
+        EVT_STATUS("done")
+        EVT_PKG("target", arg_one)
+        break;
+	case PM_TRANS_EVT_ADD_START:
+        EVT_NAME("add")
+        EVT_STATUS("start")
+        EVT_PKG("package", arg_one)
+        break;
+	case PM_TRANS_EVT_ADD_DONE:
+        EVT_NAME("add")
+        EVT_STATUS("done")
+        EVT_PKG("package", arg_one)
+        break;
+	case PM_TRANS_EVT_REMOVE_START:
+        EVT_NAME("remove")
+        EVT_STATUS("start")
+        EVT_PKG("package", arg_one)
+		break;
+	case PM_TRANS_EVT_REMOVE_DONE:
+        EVT_NAME("remove")
+        EVT_STATUS("done")
+        EVT_PKG("package", arg_one)
+		break;
+	case PM_TRANS_EVT_UPGRADE_START:
+        EVT_NAME("upgrade")
+        EVT_STATUS("start")
+        EVT_PKG("package", arg_one)
+		break;
+	case PM_TRANS_EVT_UPGRADE_DONE:
+        EVT_NAME("upgrade")
+        EVT_STATUS("done")
+        EVT_PKG("new", arg_one)
+        EVT_PKG("old", arg_two)
+		break;
+	case PM_TRANS_EVT_INTEGRITY_START:
+        EVT_NAME("integrity")
+        EVT_STATUS("start")
+		break;
+	case PM_TRANS_EVT_INTEGRITY_DONE:
+        EVT_NAME("integrity")
+        EVT_STATUS("done")
+		break;
+	case PM_TRANS_EVT_DELTA_INTEGRITY_START:
+        EVT_NAME("deltaintegrity")
+        EVT_STATUS("start")
+		break;
+	case PM_TRANS_EVT_DELTA_INTEGRITY_DONE:
+        EVT_NAME("deltaintegrity")
+        EVT_STATUS("done")
+		break;
+	case PM_TRANS_EVT_DELTA_PATCHES_START:
+        EVT_NAME("deltapatches")
+        EVT_STATUS("start")
+		break;
+	case PM_TRANS_EVT_DELTA_PATCHES_DONE:
+        EVT_NAME("deltapatches")
+        EVT_STATUS("done")
+        EVT_TEXT("pkgname", arg_one)
+        EVT_TEXT("patch", arg_two)
+		break;
+	case PM_TRANS_EVT_DELTA_PATCH_START:
+        EVT_NAME("deltapatch")
+        EVT_STATUS("start")
+		break;
+	case PM_TRANS_EVT_DELTA_PATCH_DONE:
+        EVT_NAME("deltapatch")
+        EVT_STATUS("done")
+		break;
+	case PM_TRANS_EVT_DELTA_PATCH_FAILED:
+        EVT_NAME("deltapatch")
+        EVT_STATUS("failed")
+        EVT_TEXT("error", arg_one)
+		break;
+	case PM_TRANS_EVT_SCRIPTLET_INFO:
+        EVT_NAME("scriplet")
+        EVT_STATUS("")
+        EVT_TEXT("text", arg_one)
+		break;
+    case PM_TRANS_EVT_RETRIEVE_START:
+        EVT_NAME("retrieve")
+        EVT_STATUS("start")
+        EVT_TEXT("db", arg_one)
+        break;        
+    default:
+        return;
+    }
+}
+END_TRANS_CALLBACK( event )
+    
+#define EVT_PKGLIST( NAME, LIST ) \
+    alpm_list_to_any_table( L, LIST, PMPKG_T ); \
+    lua_setfield( L, -2, NAME );
+
+BEGIN_TRANS_CALLBACK( conv, pmtransconv_t type,
+                      void *arg_one, void *arg_two, void *arg_three,
+                      int *response )
+{
+    switch ( type ) {
+    case PM_TRANS_CONV_INSTALL_IGNOREPKG:
+        EVT_NAME( "install_ignore" )
+        EVT_PKG ( "package", arg_one )
+        break;
+    case PM_TRANS_CONV_REPLACE_PKG:
+        EVT_NAME( "replace_package" )
+        EVT_PKG ( "old", arg_one )
+        EVT_PKG ( "new", arg_two )
+        EVT_TEXT( "db",  arg_three  )
+        break;
+    case PM_TRANS_CONV_CONFLICT_PKG:
+        EVT_NAME( "package_conflict" )
+        EVT_TEXT( "target",   arg_one )
+        EVT_TEXT( "local",    arg_two )
+        EVT_TEXT( "conflict", arg_three )
+        break;
+    case PM_TRANS_CONV_REMOVE_PKGS:
+        EVT_NAME   ( "remove_packages" )
+        EVT_PKGLIST( "packages", arg_one )
+        break;
+    case PM_TRANS_CONV_LOCAL_NEWER:
+        EVT_NAME( "local_newer"      )
+        EVT_PKG ( "package", arg_one )
+        break;
+    case PM_TRANS_CONV_CORRUPTED_PKG:
+        EVT_NAME( "corrupted_file" )
+        EVT_TEXT( "filename", arg_one )
+        break;
+    default:
+        return;
+    }
+    int lua_err = lua_pcall( L, 1, 0, 1 );
+    if ( lua_err != 0 ) {
+        cb_error_handler( "conv", lua_err );
+    }
+}
+
+/* We cannot use the generic transaction callback ending. */
+*response = lua_toboolean( L, -1 );
+return;
+}
+
+#define EVT_INT( KEY, INT )                     \
+    lua_pushnumber( L, INT );                   \
+    lua_setfield( L, -2, KEY );
+
+BEGIN_TRANS_CALLBACK( progress,
+                      pmtransprog_t type,
+                      const char * desc,
+                      int item_progress,
+                      int total_count,
+                      int total_pos )
+{
+    switch( type ) {
+    case PM_TRANS_PROGRESS_ADD_START:       EVT_NAME( "add"       ) break;
+    case PM_TRANS_PROGRESS_UPGRADE_START:   EVT_NAME( "upgrade"   ) break;
+    case PM_TRANS_PROGRESS_REMOVE_START:    EVT_NAME( "remove"    ) break;
+    case PM_TRANS_PROGRESS_CONFLICTS_START: EVT_NAME( "conflicts" ) break;
+    default:                                EVT_NAME( "UNKNOWN"   ) break;
+    }
+
+    EVT_TEXT( "desc",        desc );
+    EVT_INT ( "item",        item_progress );
+    EVT_INT ( "total_count", total_count );
+    EVT_INT ( "total_pos",   total_pos );
+}
+END_TRANS_CALLBACK( progress )
+
+
+#undef EVT_INT
+#undef EVT_NAME
+#undef EVT_STATUS
+#undef EVT_PKG
+#undef EVT_PKGLIST
+#undef EVT_TEXT
+
+#undef BEGIN_TRANS_CALLBACK
+#undef END_TRANS_CALLBACK
--- lualpm/trans.c
+++ lualpm/trans.c
@@ -9,87 +9,77 @@
 #include "lualpm.h"
 #include "callback.h"
 
-/* We shall keep these here, static for now.  I would prefer them
-   to be in a header file, but they are only used in this file. */
+static const char * trans_flag_names[] =
+    { "nodeps",
+      "force",
+      "nosave",
+      "",
+      "cascade",
+      "recurse",
+      "dbonly",
+      "",
+      "alldeps",
+      "downloadonly",
+      "noscriptlet",
+      "noconflicts",
+      "",
+      "needed",
+      "allexplicit",
+      "unneeded",
+      "recurseall",
+      "nolock",
+      NULL };
 
-struct event_cb_args {
-    pmtransevt_t    event;
-    void            *data1;
-    void            *data2;
-};
-
-struct conv_cb_args {
-    pmtransconv_t   type;
-    void            *data1;
-    void            *data2;
-    void            *data3;
-    int             *response;
-};
-
-struct progress_cb_args {
-    pmtransprog_t  progress_type;
-    const char    *pkg_name;
-    int            percent;
-    int            pkg_count;
-    int            pkg_current;
-};
-
-callback_key_t trans_cb_progress_key[1] = {{ "transaction progress" }};
-
-static void
-event_cb_gateway_unprotected(pmtransevt_t event, void *data1, void *data2);
-
-static void
-conversation_cb_gateway_unprotected( pmtransconv_t type,
-                                     void *data1, void *data2, void *data3,
-                                     int *response );
-
-static void
-progress_cb_gateway_unprotected( pmtransprog_t t,
-                                 const char *s, int a, int b, int c );
-
-
-/* TODO: remove this if it is as unnused as it looks -JD */
-pmtrans_t **push_pmtrans_box(lua_State *L)
+pmtransflag_t lualpm_totransflags( lua_State *L )
 {
-    pmtrans_t **box = lua_newuserdata(L, sizeof(pmtrans_t*));
-    *box = NULL;
+    pmtransflag_t result;
+    int    opt_shift;
 
-    if (luaL_newmetatable(L, "pmtrans_t")) {
-        static luaL_Reg const methods[] = {
-            { NULL,                     NULL }
-        };
-        lua_newtable(L);
-        luaL_register(L, NULL, methods);
-        lua_setfield(L, -2, "__index");
-        /*TODO DESTRUCTOR IF NEEDED*/
+    if ( ! lua_istable( L, -1 )) {
+        /* XXX: Report this error in trans_init instead? Meh. */
+        lua_pushstring( L, "Transaction flags must be provided as a list of "
+                           " flag names " );
+        lua_error( L );
+                     
     }
-    lua_setmetatable(L, -2);
 
-    return box;
+    result = 0;
+    while ( lua_next( L, -1 )) {
+        /* Ignores the key since this is a array-ish table. */
+        opt_shift = luaL_checkoption( L, -1, NULL, trans_flag_names );
+        result |= ( 1 << opt_shift );
+        lua_pop( L, 2 );
+    }
+
+    return result;
 }
 
-/* int alpm_trans_init(pmtransflag_t flags,
-                    alpm_trans_cb_event cb_event, alpm_trans_cb_conv conv,
-                    alpm_trans_cb_progress cb_progress); */
-int lalpm_trans_init(lua_State *L)
+int lalpm_trans_init ( lua_State *L )
 {
-    pmtransflag_t flags = check_transflags_table(L, 1);
-    lua_pushvalue(L, 2);
-    lua_setfield(L, LUA_REGISTRYINDEX, "lualpm: events callback table");
-    lua_pushvalue(L, 3);
-    lua_setfield(L, LUA_REGISTRYINDEX, "lualpm: conversations callback table");
-    lua_pushvalue(L, 4);
-    cb_register( L, trans_cb_progress_key );
-    const int result = alpm_trans_init(flags,
-                                       event_cb_gateway_unprotected,
-                                       conversation_cb_gateway_unprotected,
-                                       progress_cb_gateway_unprotected);
+    pmtransflag_t flags;
+    int result;
+
+    lua_getfield( L, -1, "flags" );
+    flags = lualpm_totransflags( L );
+
+    /* Registering a nil value with these is alright. In fact it is
+       better than running old callbacks. */
+
+    lua_getfield( L, -1, "eventscb" );
+    cb_register( L, &transcb_key_event );
+    lua_getfield( L, -1, "convcb" );
+    cb_register( L, &transcb_key_conv );
+    lua_getfield( L, -1, "progresscb" );
+    cb_register( L, &transcb_key_progress );
+    
+    result = alpm_trans_init( flags,
+                              transcb_cfunc_event,
+                              transcb_cfunc_conv,
+                              transcb_cfunc_progress );
     lua_pushnumber(L, result);
     return 1;
 }
 
-/* int alpm_trans_prepare(alpm_list_t **data); */
 int lalpm_trans_prepare(lua_State *L)
 {
     luaL_checktype(L, 1, LUA_TTABLE);
@@ -364,248 +354,3 @@ pmfileconflict_t **push_pmfileconflict_box(lua_State *L)
     return box;
 }
 
-/* TRANSACTION CALLBACKS ****************************************************/
-
-static int
-event_cb_gateway_protected(lua_State *L)
-{
-    struct event_cb_args *args = lua_touserdata(L, 1);
-    lua_getfield(L, LUA_REGISTRYINDEX, "lualpm: events callback table");
-    switch (args->event) {
-        pmpkg_t **box;
-        pmpkg_t **box1;
-#define f(x) case PM_TRANS_EVT_ ## x: lua_getfield(L, -1, "T_E_" #x)
-#define bn(b) if (b == NULL) { \
-                    lua_pushnil(L); }
-#define g(x, y) lua_call(L, x, y); break
-        f(CHECKDEPS_START);
-            g(0, 0);
-        f(FILECONFLICTS_START);
-            g(0, 0);
-        f(RESOLVEDEPS_START);
-            g(0, 0);
-        f(INTERCONFLICTS_START);
-            g(0, 0);
-        f(ADD_START);
-            box = push_pmpkg_box(L);
-            *box = args->data1;
-            bn(*box);
-            g(1, 0);
-        f(ADD_DONE);
-            box = push_pmpkg_box(L);
-            *box = args->data1;
-            bn(*box);
-            g(1, 0);
-        f(REMOVE_START);
-            box = push_pmpkg_box(L);
-            *box = args->data1;
-            bn(*box);
-            g(1, 0);
-        f(REMOVE_DONE);
-            box = push_pmpkg_box(L);
-            *box  = args->data1;
-            bn(*box);
-            g(1, 0);
-        f(UPGRADE_START);
-            box = push_pmpkg_box(L);
-            *box = args->data1;
-            bn(*box);
-            g(1, 0);
-        f(UPGRADE_DONE);
-            box = push_pmpkg_box(L);
-            *box = args->data1;
-            bn(*box);
-            box1 = push_pmpkg_box(L);
-            *box1 = args->data2;
-            bn(*box1);
-            g(2, 0);
-        f(INTEGRITY_START);
-            g(0, 0);
-        f(DELTA_INTEGRITY_START);
-            g(0, 0);
-        f(DELTA_PATCHES_START);
-            g(0, 0);
-        f(DELTA_PATCH_START);
-            push_string(L, (char *)args->data1);
-            push_string(L, (char *)args->data2);
-            g(2, 0);
-        f(DELTA_PATCH_DONE);
-            g(0, 0);
-        f(DELTA_PATCH_FAILED);
-            g(0, 0);
-        f(SCRIPTLET_INFO);
-            push_string(L, (char *)args->data1);
-            g(1, 0);
-        f(RETRIEVE_START);
-            push_string(L, (char *)args->data1);
-            g(1, 0);
-#undef f
-#undef bn
-#undef g
-#define f(x) case PM_TRANS_EVT_ ## x:
-            f(FILECONFLICTS_DONE)
-            f(CHECKDEPS_DONE)
-            f(RESOLVEDEPS_DONE)
-            f(INTERCONFLICTS_DONE)
-            f(INTEGRITY_DONE)
-            f(DELTA_INTEGRITY_DONE)
-            f(DELTA_PATCHES_DONE)
-            break;
-#undef f
-        default:
-            assert(0 && "[BUG] unexpected pmtransevt_t");
-    }
-
-    return 0;
-}
-
-static void
-event_cb_gateway_unprotected(pmtransevt_t event, void *data1, void *data2)
-{
-    lua_State *L = GlobalState;
-    struct event_cb_args args[1];
-    int err;
-    assert(L && "[BUG] no global Lua state in transaction event callback");
-    args->event = event;
-    args->data1 = data1;
-    args->data2 = data2;
-    err = lua_cpcall(L, event_cb_gateway_protected, args);
-    if (err) {
-        cb_error_handler( "event", err );
-    }
-}
-
-static int
-conversation_cb_gateway_protected(lua_State *L)
-{
-    struct conv_cb_args *args = lua_touserdata(L, 1);
-    lua_getfield(L, LUA_REGISTRYINDEX, "lualpm: conversations callback table");
-    switch (args->type) {
-        pmpkg_t **box;
-        pmpkg_t **box1;
-#define f(x) case PM_TRANS_CONV_  ## x: lua_getfield(L, -1, "T_C_" #x)
-#define g(x, y) lua_call(L, x, y); \
-                if (args->response) { \
-                args->response[0] = lua_toboolean(L, -1); \
-                } \
-                lua_pop(L, 1); \
-                break
-#define bn(b) if (b == NULL) { \
-                    lua_pushnil(L); }
-        f(INSTALL_IGNOREPKG);
-            box = push_pmpkg_box(L);
-            *box = args->data1;
-            bn(*box);
-            g(1, 1);
-        f(REPLACE_PKG);
-            box = push_pmpkg_box(L);
-            *box = args->data1;
-            bn(*box);
-            box1 = push_pmpkg_box(L);
-            *box1 = args->data2;
-            bn(*box1);
-            push_string(L, args->data3);
-            g(3, 1);
-        f(CONFLICT_PKG);
-            push_string(L, args->data1);
-            push_string(L, args->data2);
-            push_string(L, args->data3);
-            g(3, 1);
-        f(CORRUPTED_PKG);
-            push_string(L, args->data1);
-            g(1, 1);
-        f(LOCAL_NEWER);
-            box = push_pmpkg_box(L);
-            *box = args->data1;
-            bn(*box);
-            g(1, 1);
-        f(REMOVE_PKGS);
-            alpm_list_to_any_table(L, args->data1, PMPKG_T);
-            g(1, 1);
-#undef f
-#undef g
-#undef bn
-        default:
-            assert(0 && "[BUG] unexpected pmtransconv_t");
-    }
-
-    return 0;
-}
-
-static void
-conversation_cb_gateway_unprotected(pmtransconv_t type, void *data1, void *data2, void *data3, int *response)
-{
-    lua_State *L = GlobalState;
-    struct conv_cb_args args[1];
-    int err;
-    assert(L && "[BUG] no global Lua state in conversation event callback");
-    args->type = type;
-    args->data1 = data1;
-    args->data2 = data2;
-    args->data3 = data3;
-    args->response = response;
-    err = lua_cpcall(L, conversation_cb_gateway_protected, args);
-    if (err) {
-        cb_error_handler( "conversation", err );
-    }
-}
-
-static int
-push_pmtransprog(lua_State *L, pmtransprog_t t)
-{
-    switch (t) {
-#define f(x) case PM_TRANS_PROGRESS_ ## x: return push_string(L, "T_P_" #x)
-        f(ADD_START);
-        f(UPGRADE_START);
-        f(REMOVE_START);
-        f(CONFLICTS_START);
-#undef f
-    default:
-        assert(0 && "[BUG] unexpected pmtransprog_t");
-    }
-    return 0;
-}
-
-/* This is a protected gateway for the progress callback.  It receives
- * the progress callback arguments on the stack as a lightuserdata
- * pointing to a struct progress_cb_args. */
-static int
-progress_cb_gateway_protected(lua_State *L)
-{
-    struct progress_cb_args *args = lua_touserdata(L, 1);
-
-    /* We'll look for a callback to call in the registry. */
-    cb_lookup(trans_cb_progress_key);
-    push_pmtransprog(L, args->progress_type);
-    push_string(L, args->pkg_name);
-    lua_pushnumber(L, args->percent);
-    lua_pushnumber(L, args->pkg_count);
-    lua_pushnumber(L, args->pkg_current);
-    lua_call(L, 5, 0);
-
-    return 0;
-}
-
-/* This is called by libalpm whenever a transaction progress event
- * occurs.  The context of the call is such that no Lua API functions
- * which may throw errors are allowed since that would cause a setjmp
- * past libalpm's call frames.  Instead we wrap the arguments into a
- * struct and transfer control to a protected gateway for further
- * processing. */
-static void
-progress_cb_gateway_unprotected(pmtransprog_t t, const char *s, int a, int b, int c)
-{
-    lua_State *L = GlobalState;
-    struct progress_cb_args args[1];
-    int err;
-    assert(L && "[BUG] no global Lua state in progress callback");
-    args->progress_type = t;
-    args->pkg_name = s;
-    args->percent = a;
-    args->pkg_count = b;
-    args->pkg_current = c;
-    err = lua_cpcall(L, progress_cb_gateway_protected, args);
-    if (err) {
-        cb_error_handler( "progress", err );
-    }
-}
